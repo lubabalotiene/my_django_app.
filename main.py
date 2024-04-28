37c1320217c16a814d45d18455df1e113e49cf3b
lubabalotiene <lubabalotiene@gmail.com>
2024-04-28 09:31:17 +0200

    Added docstrings to main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+
+"""
+Main script to run the political candidate's website.
+
+This script serves as the entry point for the web application.
+
+Functions:
+- run_website(): Starts the web server to serve the candidate's website.
+"""
+
+

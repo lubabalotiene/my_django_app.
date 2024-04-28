4969a138ec8f1480b986afcf94d0be49019072cc
lubabalotiene <lubabalotiene@gmail.com>
2024-04-28 09:27:18 +0200

    Added docstrings to event_info.py

---
+++ event_info.py
@@ -0,0 +1,11 @@
+
+# Docstring
+
+"""
+Module to retrieve and display information about upcoming campaign events.
+
+Functions:
+- get_upcoming_events(): Retrieves a list of upcoming events.
+- get_event_details(event_id): Retrieves details about a specific event.
+"""
+

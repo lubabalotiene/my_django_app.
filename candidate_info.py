f3dd001ead16e225c08affae3fe114536d298085
lubabalotiene <lubabalotiene@gmail.com>
2024-04-28 09:25:10 +0200

    Added docstrings to candidate_info.py

---
+++ candidate_info.py
@@ -0,0 +1,11 @@
+
+#Docstring
+
+"""
+Module to retrieve and display information about the political candidate.
+
+Functions:
+- get_candidate_name(): Retrieves the candidate's name.
+- get_candidate_bio(): Retrieves the candidate's biography.
+"""
+

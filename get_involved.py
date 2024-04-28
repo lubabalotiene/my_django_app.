fbe560d3f3e9c4fa4067cd077a3d20e0e146adae
lubabalotiene <lubabalotiene@gmail.com>
2024-04-28 09:27:53 +0200

    Added docstrings to get_involved.py

---
+++ get_involved.py
@@ -0,0 +1,11 @@
+
+# Docstring
+
+"""
+Module to provide information on how to get involved with the campaign.
+
+Functions:
+- get_volunteer_opportunities(): Retrieves volunteer opportunities.
+- get_donation_information(): Retrieves information on how to donate to the campaign.
+"""
+
